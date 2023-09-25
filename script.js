e1144620fc5633954416d97de6dccc0575847ff8
nataliekubasek <143412345+nataliekubasek@users.noreply.github.com>
2023-09-24 23:31:01 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function quizQuestion() {
-  var password = document.getElementById("passwordBox");
-  var passwordText = password.value.toUpperCase();
-  if (passwordText == "3:49") {
-    return true;
-  }
-  alert("That is incorrect, try again!");
-  return false;
-}
